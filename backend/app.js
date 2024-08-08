d1bf1589a1b2e1ac192054dcea60697db56af300
moizsheraz <user146712@gmail.com>
2024-08-08 21:07:55 +0500

    config for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,51 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { config as cloudinaryConfig } from 'cloudinary';
+import errorMiddleware from './middleware/error.js';
 import userRouter from './routes/user.route.js';
 import eventRouter from './routes/event.route.js';
 import teamRouter from './routes/team.route.js';
-import {v2 as cloudinary} from 'cloudinary';
-import errorMiddleware from './middleware/error.js';
 
-dotenv.config({
-    path: './config/config.env'
-})
+const app = express();
 
+const corsOptions = {
+  origin: "*",
+};
 
-cloudinary.config({
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+cloudinaryConfig({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-const app = express();
-app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the User Registration API");
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json("Health checking");
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/event', eventRouter);
 app.use('/api/team', teamRouter);
 
 app.use(errorMiddleware);
-app.get("/", (req, res) => {
-    res.send("Welcome");
-  });
-  app.get("/health", (req, res) => {
-    res.status(200).json("Health checking");
-  });
+
 export default app;
