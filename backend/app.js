71f971f34fdbc06ea9bb3761922ce304c164d357
moizsheraz <user146712@gmail.com>
2024-08-08 20:50:45 +0500

    config for deployment

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js';
+import eventRouter from './routes/event.route.js';
+import teamRouter from './routes/team.route.js';
+import {v2 as cloudinary} from 'cloudinary';
+import errorMiddleware from './middleware/error.js';
+
+dotenv.config({
+    path: './config/config.env'
+})
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/user', userRouter);
+app.use('/api/event', eventRouter);
+app.use('/api/team', teamRouter);
+
+app.use(errorMiddleware);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDatabase } from './database/databaseConnection.js';
-import cookieParser from 'cookie-parser';
-import userRouter from './routes/user.route.js';
-import eventRouter from './routes/event.route.js';
-import teamRouter from './routes/team.route.js';
-import {v2 as cloudinary} from 'cloudinary';
-import errorMiddleware from './middleware/error.js';
+import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config({
-    path: './config/config.env'
-})
+dotenv.config();
 
-connectDatabase();
+const DB = process.env.DATABASE_URI
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+const connectToMongodb = async () => {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error in connecting to Mongodb", error.message);
+  }
+};
 
-
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/api/user', userRouter);
-app.use('/api/event', eventRouter);
-app.use('/api/team', teamRouter);
-
-app.use(errorMiddleware);
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 8000}`);
-});
-
-module.exports = app;
+connectToMongodb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at  port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => console.log("MongoDB connection failed !!!", error.message));
+  
